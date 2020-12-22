eb5531a5930cfae873b87fde450210ff7babbb79
Immunarray <pitts.justin@gmail.com>
2020-12-22 06:19:03 +0000

    spelling and env setup

--- zps/customer/models.py
+++ zps/customer/models.py
@@ -2,10 +2,10 @@ from django.db import models, AutoField
 
 
 class Customer(models.Model):
-    customer_status = models.
+    customer_status = models.BinaryField
     customer_name = models.CharField(max_length=500, help_text="Customer Name")
-    customer_createdate = models.DateField()
-    customer_last_modifed_date =
+    customer_create_date = models.DateField()
+    customer_last_modified_date = models.DateField()
     customer_address
     customer_created_by
     customer_updated_by_
