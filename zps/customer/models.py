ca30033dcc905e5fbba0cc7fa5f858e5f205e35e
Immunarray <pitts.justin@gmail.com>
2020-12-22 05:00:53 +0000

    spelling corrections

--- zps/customer/models.py
+++ zps/customer/models.py
@@ -1,9 +1,13 @@
 from django.db import models, AutoField
 
+
 class Customer(models.Model):
-    customer_pk = AutoField.BigAutoField() # Primany Key for Customer
-    customer_name = models.CharField(max_lenght=500) # Customer Name
-    create_date = models.DateTimeField('date created') # Date customer was created non editable
-    customer_status = models.BooleanField() # Customer status 
-    customer_credit_terms = [('COD','Cash On Delivery'),('N30','Net 30'), ('N10','Net 10')]
-# Create your models here.
+    customer_status = models.
+    customer_name = models.CharField(max_length=500, help_text="Customer Name")
+    customer_createdate = models.DateField()
+    customer_last_modifed_date =
+    customer_address
+    customer_created_by
+    customer_updated_by_
+
+# Create your models here.:q!:
