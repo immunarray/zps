b9207952dca8f44b2863c8e031051b07afd04618
Immunarray <pitts.justin@gmail.com>
2020-12-26 00:15:58 -0500

    adding fields to customer model

--- zps/customer/models.py
+++ zps/customer/models.py
@@ -6,8 +6,8 @@ class Customer(models.Model):
     customer_name = models.CharField(max_length=500, help_text="Customer Name")
     customer_create_date = models.DateField()
     customer_last_modified_date = models.DateField()
-    customer_address
-    customer_created_by
-    customer_updated_by_
+    customer_address = models.CharField(max_length=500, help_text="Customer Address")
+    customer_created_by = models.CharField(max_length=200, help_text="Created By")
+    customer_updated_by = models.DateField()
 
 # Create your models here.:q!:
